65b230a8b3129a05bf38812680e43d865756cdb0
Ward Cunningham <ward@c2.com>
2019-02-28 20:43:44 -0800

    sync assets

--- sync.sh
+++ sync.sh
@@ -2,4 +2,5 @@
 # usage: sh sync.sh
 
 rsync -avz ~/.wiki/growing.localhost/pages/ root@bay.wiki.org:.wiki/growing.bay.wiki.org/pages/
+rsync -avz ~/.wiki/growing.localhost/assets/ root@bay.wiki.org:.wiki/growing.bay.wiki.org/assets/
 ssh root@bay.wiki.org 'cd .wiki/growing.bay.wiki.org/status; rm -f sitemap.*'
